d2d34ef38f6ece5bc697ee89310041729b750852
Jeremy White <jeremywhite761@gmail.com>
2025-10-28 01:55:21 -0400

    Updated Coverage and Coveralls implmentation

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # iHike 
 
 ![Django CI/CD](https://github.com/UltimateBoss80/iHike/actions/workflows/iHikeDjango.yml/badge.svg)
+[![Coverage Status](https://coveralls.io/repos/github/UltimateBoss80/iHike/badge.svg?branch=main)](https://coveralls.io/github/UltimateBoss80/iHike?branch=main)
--- backend/ihike_backend/tests.py
+++ backend/ihike_backend/tests.py
@@ -1,7 +1,18 @@
 from django.test import TestCase
+from django.test import override_settings
+from ihike_backend.pagination import StandardResultsSetPagination
 
 
 class RootHealthTest(TestCase):
     def test_root_health_ok(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
+
+
+class PaginationTest(TestCase):
+    @override_settings()
+    def test_pagination_defaults(self):
+        paginator = StandardResultsSetPagination()
+        self.assertEqual(paginator.page_size, 200)
+        self.assertEqual(paginator.page_size_query_param, "page_size")
+        self.assertEqual(paginator.max_page_size, 5040)
